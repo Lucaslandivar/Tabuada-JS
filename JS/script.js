daecacd7e5554fb2c598888aca2c123258da6fb7
Lucas Landivar <119813555+Lucaslandivar@users.noreply.github.com>
2023-07-02 16:20:17 -0300

    First part of the JavaScript

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+// Selecting the elements
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplication");
+
+// Functions
+
+// Events
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const multiplicationNumber = +numberInput.value;
+
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    if(!multiplicationNumber || !multiplicatorNumber) return;
+
+    console.log(multiplicationNumber, multiplicationNumber);
+}) 
