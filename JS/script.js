3acde4eb0da151f3f213f9a31fcad71418518ec3
Lucas Landivar <119813555+Lucaslandivar@users.noreply.github.com>
2023-07-03 07:29:33 -0300

    I added a new function
    The new function make this project more usefull, allowing the user to generate a square root equations too.

--- JS/script.js
+++ JS/script.js
@@ -3,45 +3,71 @@ const multiplicationForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number");
 const multiplicationInput = document.querySelector("#multiplicator");
 
-const multiplicationTitle = document.querySelector(
-  "#multiplication-title span"
-);
-const multiplicationTable = document.querySelector(
-  "#multiplication-operations"
-);
+const multiplicationTitle = document.querySelector("#multiplication-title span");
+const multiplicationTable = document.querySelector("#multiplication-operations");
 
+const squareRootForm = document.querySelector("#squareRoot-form");
+const squareRootNumberInput = document.querySelector("#squareRoot-number");
+const squareRootTable = document.querySelector("#squareRoot-operations");
+const squareRootTitle = document.querySelector("#squareRoot-title span");
 
 // Functions
-const createTable = (number, multiplicatorNumber) => {
-    multiplicationTable.innerHTML = "";
-  
-    for (i = 1; i <= multiplicatorNumber; i++) {
-      const result = number * i;
-  
-      const template = `<div class="row">
-              <div class="operation">${number} x ${i} = </div>
-              <div class="result">${result}</div>
-          </div>`;
-  
-      const parser = new DOMParser();
-      const htmlTemplate = parser.parseFromString(template, "text/html");
-      const row = htmlTemplate.querySelector(".row");
-  
-      multiplicationTable.appendChild(row);
-    }
-  
-    multiplicationTitle.innerText = number;
-  };
+const createMultiplicationTable = (number, multiplicatorNumber) => {
+  multiplicationTable.innerHTML = "";
+
+  for (let i = 1; i <= multiplicatorNumber; i++) {
+    const result = number * i;
+
+    const template = `
+      <div class="row">
+        <div class="operation">${number} x ${i} = </div>
+        <div class="result">${result}</div>
+      </div>
+    `;
+
+    multiplicationTable.innerHTML += template;
+  }
+
+  multiplicationTitle.innerText = number;
+};
+
+const createSquareRootTable = (number) => {
+  squareRootTable.innerHTML = "";
+
+  for (let i = 1; i <= number; i++) {
+    const squareRoot = Math.sqrt(i);
+
+    const template = `
+      <div class="row">
+        <div class="operation">√${i} = </div>
+        <div class="result">${squareRoot.toFixed(2)}</div>
+      </div>
+    `;
+
+    squareRootTable.innerHTML += template;
+  }
+
+  squareRootTitle.innerText = number;
+};
 
 // Events
 multiplicationForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-  
-    const multiplicationNumber = numberInput.value;
-  
-    const multiplicatorNumber = +multiplicationInput.value;
-  
-    if (!multiplicationNumber || !multiplicatorNumber) return;
-  
-    createTable(multiplicationNumber, multiplicatorNumber);
-  });
+  e.preventDefault();
+
+  const multiplicationNumber = numberInput.value;
+  const multiplicatorNumber = +multiplicationInput.value;
+
+  if (!multiplicationNumber || !multiplicatorNumber) return;
+
+  createMultiplicationTable(multiplicationNumber, multiplicatorNumber);
+});
+
+squareRootForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const squareRootNumber = squareRootNumberInput.value;
+
+  if (!squareRootNumber) return;
+
+  createSquareRootTable(squareRootNumber);
+});
